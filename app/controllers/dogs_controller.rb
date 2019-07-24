7637d15bb26d69d499e7f79ee41f71af31cd572c
Jinzi Yan <stephanie.jinzi.yan@gmail.com>
2019-07-24 15:32:00 -0700

    pair programming

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,37 @@
 class DogsController < ApplicationController
+    def index
+        @dogs = Dog.all
+        
+    end
+
+    def new
+        @dog = Dog.new
+    end
+    
+    def create
+
+        @dog = Dog.create(dog_params)
+        redirect_to @dog
+    end
+    
+    def show
+       @dog = Dog.find(params[:id])
+    end
+
+    def update
+       @dog = Dog.find(params[:id])
+       @dog.update(dog_params)
+       redirect_to @dog
+    end
+
+    def edit
+        @dog = Dog.find(params[:id])
+    end
+
+    private
+
+    def dog_params
+        params.require(:dog).permit(:name,:breed,:age)
+    end
+
 end
--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,2 +1,8 @@
 class Dog < ApplicationRecord
+    has_many :employees
+
+    def employee_count
+        self.employees.count
+    end
+
 end
