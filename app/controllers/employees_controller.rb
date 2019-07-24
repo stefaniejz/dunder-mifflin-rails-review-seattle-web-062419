c136ac845e462c3d303d1f12c62d945301483df0
Jinzi Yan <stephanie.jinzi.yan@gmail.com>
2019-07-24 15:44:38 -0700

    final

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -32,7 +32,7 @@ class EmployeesController < ApplicationController
 
     private
     def employee_params
-        params.require(:employee).permit(:first_name,:last_name,:alias,:title,:office,:img_url)
+        params.require(:employee).permit(:first_name,:last_name,:alias,:title,:office,:img_url, :dog_id)
     end
 
 end
